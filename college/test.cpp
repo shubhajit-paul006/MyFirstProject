dc91797181f6d1bc85f89ff7742b3a4348752cc8
Shubhajit Paul <shubhajitpaul102@gmail.com>
2026-08-17 23:39:24 +0530

    Learning how to take line as input

---
+++ college/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string name;
+	cout<<"enter your name: ";
+	getline(cin,name);
+	cout<<"name is : "<<name;
+	return 0;
+}
